c9edff3823a39cc48a76eb97f12bc581d5d010f3
Camry.Chen <camry.chen@foxmail.com>
2022-09-22 14:47:08 +0800

    Fixed Caller for /glog/global.go file.

--- glog/value.go
+++ glog/value.go
@@ -36,6 +36,10 @@ func Caller(depth int) Valuer {
             depth++
             _, file, line, _ = runtime.Caller(depth)
         }
+        if strings.LastIndex(file, "/glog/global.go") > 0 {
+            depth++
+            _, file, line, _ = runtime.Caller(depth)
+        }
         if strings.LastIndex(file, "/glog/helper.go") > 0 {
             depth++
             _, file, line, _ = runtime.Caller(depth)
