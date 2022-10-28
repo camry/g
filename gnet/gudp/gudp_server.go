c8178a3b5be46802bd68c4906f4d11d8bf244466
Camry.Chen <camry.chen@foxmail.com>
2022-10-28 14:31:48 +0800

    优化UDP组件

--- gnet/gudp/gudp_server.go
+++ gnet/gudp/gudp_server.go
@@ -10,6 +10,7 @@ import (
 
 // Server 定义 UDP 服务器。
 type Server struct {
+    err     error
     mu      sync.Mutex  // 用于 Server.Conn 并发安全。
     conn    *Conn       // UDP 服务器连接对象。
     network string      // UDP 服务器网络协议。
@@ -24,28 +25,19 @@ func NewServer(address string, handler func(*Conn)) *Server {
         address: address,
         handler: handler,
     }
+    srv.err = srv.listen()
     return srv
 }
 
 // Run 启动 UDP 服务器。
 func (s *Server) Run(ctx context.Context) (err error) {
+    if s.err != nil {
+        return s.err
+    }
     if s.handler == nil {
         err = errors.New("start running failed: socket handler not defined")
         return
     }
-    addr, err := net.ResolveUDPAddr(s.network, s.address)
-    if err != nil {
-        err = fmt.Errorf(`net.ResolveUDPAddr failed for address "%s"`, s.address)
-        return
-    }
-    conn, err := net.ListenUDP(s.network, addr)
-    if err != nil {
-        err = fmt.Errorf(`net.ListenUDP failed for address "%s"`, s.address)
-        return
-    }
-    s.mu.Lock()
-    s.conn = NewConnByNetConn(conn)
-    s.mu.Unlock()
     s.handler(s.conn)
     return nil
 }
@@ -55,6 +47,22 @@ func (s *Server) Close(ctx context.Context) error {
     s.mu.Lock()
     defer s.mu.Unlock()
     return s.conn.Close()
+}
+
+// listen 网络监听。
+func (s *Server) listen() error {
+    addr, err := net.ResolveUDPAddr(s.network, s.address)
+    if err != nil {
+        return fmt.Errorf(`net.ResolveUDPAddr failed for address "%s"`, s.address)
+    }
+    conn, err := net.ListenUDP(s.network, addr)
+    if err != nil {
+        return fmt.Errorf(`net.ListenUDP failed for address "%s"`, s.address)
+    }
+    s.mu.Lock()
+    s.conn = NewConnByNetConn(conn)
+    s.mu.Unlock()
+    return nil
 }
 
 // Conn UDP 服务器连接对象
