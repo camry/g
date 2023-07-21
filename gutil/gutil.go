97b0d1afda60ecb0411d4ed8f7abdb86d22b7be3
Camry.Chen <camry.chen@foxmail.com>
2023-07-21 10:55:10 +0800

    UPDATE gutil.InArray

--- gutil/gutil.go
+++ gutil/gutil.go
@@ -1,18 +1,11 @@
 package gutil
 
-import "reflect"
-
 // InArray 将在数组中搜索任意类型的元素，返回匹配元素的布尔值。
-// needle 是要搜索的元素，haystack 是要搜索的值的切片。
-func InArray(needle any, haystack any) bool {
-    switch reflect.TypeOf(haystack).Kind() {
-    case reflect.Slice:
-        s := reflect.ValueOf(haystack)
-
-        for i := 0; i < s.Len(); i++ {
-            if reflect.DeepEqual(needle, s.Index(i).Interface()) == true {
-                return true
-            }
+// element 是要搜索的元素，collection 是要搜索的值的切片。
+func InArray[T comparable](element T, collection []T) bool {
+    for _, item := range collection {
+        if item == element {
+            return true
         }
     }
     return false
